bd6ed62043407e812566d63d66794a15ccc4e3c1
George Shtyfman <gosha-gosha.sht@mail.ru>
2026-02-23 16:30:01 +0500

    add: код для ргр 1. лекция 19 02

---
+++ first_assignment_19_02.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+class BooleanMatrix:
+    def __init__(self, np_matrix):
+        self.matrix = np_matrix
+
+    #дизъюнкция
+    def disjunction(self, other):
+        out = np.zeros_like(self.matrix)
+        m, n = self.matrix.shape
+
+        for i in range(m):
+            for j in range(n):
+                out[i, j] = self.matrix[i, j] | other.matrix[i, j]
+
+        return BooleanMatrix(out)
+
+    #транспонирование
+    def transpose(self):
+        self.matrix = np.transpose(self.matrix)
+
+    #инверсирование
+    def inverse(self):
+        m, n = self.matrix.shape
+        for i in range(m):
+            for j in range(n):
+                self.matrix[i, j] = 1 - self.matrix[i, j]
+
+    #умножение матриц
+    def __mul__(self, other):
+        m, n = self.matrix.shape #кол во строк и столбцов в 1ой матрице
+        p = self.matrix.shape[1] #кол во столбоцв в 2ой матрице
+
+        out = np.zeros((m, p), dtype=int)
+
+        for i in range(m):
+            for j in range(p):
+                for k in range(n):
+                    if self.matrix[i, k] and other.matrix[k, j]:
+                        out[i, j] = 1
+                        break
+
+        return BooleanMatrix(out)
+
+    #вычитание матриц
+    def __sub__(self, other):
+        out = np.zeros_like(self.matrix)
+        m, n = self.matrix.shape
+
+        for i in range(m):
+            for j in range(n):
+                out[i, j] = self.matrix[i, j] & (1 - other.matrix[i, j])
+
+        return BooleanMatrix(out)
+
+a = BooleanMatrix(np.matrix('1, 0; 0, 1'))
+b = BooleanMatrix(np.matrix('0, 1; 1, 0'))
+d = a * b
+
+print(d.matrix)
+
+d.inverse()
+print(d.matrix)
+
+d.transpose()
+print(d.matrix)
+
+c = a.disjunction(b)
+print(c.matrix)
+
+e = a - b
+print(e.matrix)
